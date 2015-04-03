9b3594ae23f26cf3e9396291b80805152c3c8930
Swaraj Banerjee <swarajban@gmail.com>
2015-04-03 09:33:09 -0700

    nearly finished with build steps

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
+var inject = require('gulp-inject');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var inject = require('gulp-inject');
 var livereload = require('gulp-livereload');
 var sass = require('gulp-sass');
 
@@ -21,12 +26,8 @@ var paths = {
       jsxComponents + 'helloMessage.jsx',
       jsxSource + 'app.jsx'
     ],
-    sass: [
-      sassSource + 'main.scss'
-    ],
-    html: [
-      src + 'index.html'
-    ]
+    sass: sassSource + 'app.scss',
+    html: src + 'index.html'
   },
   vendor: {
     js: [
@@ -46,10 +47,38 @@ var paths = {
   }
 };
 
-gulp.task('html', function () {
-  return gulp.src(paths.src.html)
-    .pipe(gulp.dest(paths.dist.html));
+gulp.task('html.dev', ['js', 'css'], function () {
+  return injectHTML([
+    'css/app.css',
+    'js/vendor.js',
+    'js/app.js'
+  ], 'index.dev.html');
 });
+
+gulp.task('html.min', ['js', 'css'], function () {
+  return injectHTML([
+    'css/app.min.css',
+    'js/vendor.min.js',
+    'js/app.min.js'
+  ], 'index.html');
+});
+
+
+gulp.task('html', ['html.dev', 'html.min']);
+
+function injectHTML (sourcePaths, outputName) {
+  var injectSources = gulp.src(sourcePaths, {
+    read: false,
+    cwd: dist
+  });
+
+  return gulp.src(paths.src.html)
+    .pipe(inject(injectSources, {
+      addRootSlash: false
+    }))
+    .pipe(rename(outputName))
+    .pipe(gulp.dest(paths.dist.html));
+}
 
 
 gulp.task('vendor.js', function () {
@@ -71,6 +100,11 @@ gulp.task('src.js', function () {
     .pipe(concat('app.js'))
     .pipe(babel())
     .pipe(gulp.dest(paths.dist.js))
+    .pipe(uglify({
+      mangle: true
+    }))
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest(paths.dist.js))
     .pipe(livereload());
 });
 
@@ -86,16 +120,25 @@ gulp.task('src.sass', function () {
   return gulp.src(paths.src.sass)
     .pipe(sass())
     .pipe(gulp.dest(paths.dist.css))
+    .pipe(rename('app.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.dist.css))
     .pipe(livereload());
 });
 
+gulp.task('css', ['src.sass']);
 
-gulp.task('watch', ['watch.js'], function () {
+gulp.task('watch.css', ['css'], function () {
+  gulp.watch(paths.src.sass, ['src.sass']);
+});
+
+
+gulp.task('watch', ['watch.js', 'watch.css'], function () {
   return livereload.listen();
 });
 
 
-gulp.task('build', ['js', 'html']);
+gulp.task('build', ['js', 'css', 'html']);
 
 gulp.task('default', ['build']);
 
